026095ef10104ab5b06e9f05ba308fa3258b4bf4
bjmashibing <alanpppbox@gmail.com>
2019-04-25 21:46:20 +0800

    添加键盘处理，根据上下左右箭头的按键状态，判定坦克的移动方向

--- tank/src/com/caoliang/tank/TankFrame.java
+++ tank/src/com/caoliang/tank/TankFrame.java
@@ -25,25 +25,68 @@ public class TankFrame extends Frame{
 
         });
 
-        addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                System.out.println("key pressed");
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                System.out.println("key released");
-            }
-        });
+        addKeyListener(new MyKeyListener());
     }
 
     @Override
     public void paint(Graphics g) {
-        System.out.println("paint");
         g.fillRect(x, y, 50, 50);
-        x += 10;
+        //x += 10;
         //y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+
+        boolean bL = false;
+
+        boolean bR = false;
+
+        boolean bU = false;
+
+        boolean bD = false;
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
 }
