b83407c6d3e877994752c73c7f01dd79f6cea7c3
bjmashibing <alanpppbox@gmail.com>
2019-04-30 20:47:32 +0800

    自己的项目,tank换成图片

--- tank/src/com/caoliang/tank/Tank.java
+++ tank/src/com/caoliang/tank/Tank.java
@@ -1,5 +1,7 @@
 package com.caoliang.tank;
 
+import com.mashibing.tank.ResourceMgr;
+
 import java.awt.*;
 
 public class Tank {
@@ -20,10 +22,20 @@ public class Tank {
     }
 
     public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.YELLOW);
-        g.fillRect(x, y, 50, 50);
-        g.setColor(c);
+        switch(dir) {
+            case LEFT:
+                g.drawImage(ResourceMgr.tankL, x, y, null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.tankU, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.tankR, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.tankD, x, y, null);
+                break;
+        }
 
         moving();
     }
