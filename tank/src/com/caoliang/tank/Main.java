237a42b20f8674254ea7a45dfc839a9ae247b31c
Alex119793898 <119793898@qq.com>
2019-05-05 21:33:38 +0800

    加入配置文件，让程序更灵活

--- tank/src/com/caoliang/tank/Main.java
+++ tank/src/com/caoliang/tank/Main.java
@@ -1,13 +1,19 @@
 package com.caoliang.tank;
 
+import com.mashibing.tank.Audio;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
 
+        Integer initTankCount = Integer.parseInt((String) PropertyMgr.get("initTankCount"));
         //初始化地方坦克
-        for(int i =0; i<5; i++){
+        for(int i =0; i<initTankCount; i++){
             tankFrame.tanks.add(new Tank(50 + 80 * i, 100, Dir.DOWN, Group.Bad, tankFrame));
         }
+
+        //游戏音乐
+        //new Thread(()->new Audio("audio/war1.wav").loop()).start();
 
         while (true){
             Thread.sleep(25);
