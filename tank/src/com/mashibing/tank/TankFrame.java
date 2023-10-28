41b6556f45bef9f0c16a7b0c69e56cbbcc1e6cfe
Alex119793898 <119793898@qq.com>
2023-10-28 13:04:19 +0800

    文件整理

---
+++ tank/src/com/mashibing/tank/TankFrame.java
@@ -0,0 +1,36 @@
+package com.mashibing.tank;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+	
+	int x = 200, y = 200;
+	
+	public TankFrame() {
+		setSize(800, 600);
+		setResizable(false);
+		setTitle("tank war");
+		setVisible(true);
+		
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+		});
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		System.out.println("paint");
+		g.fillRect(x, y, 50, 50);
+		x += 10;
+		y += 10;
+	}
+	
+}
